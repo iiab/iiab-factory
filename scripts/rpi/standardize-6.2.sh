0ce0016323d21a26eee19fa35ca812621cfd059e
George Hunt <georgejhunt@gmail.com>
2017-03-21 12:13:40 -0700

    chase after changes with links

--- scripts/rpi/standardize-6.2.sh
+++ scripts/rpi/standardize-6.2.sh
@@ -13,7 +13,7 @@ git pull origin release-6.2
 
 # make sure the base operating system is updated -- the base image may be stale
 apt-get -y update
-apt-get -y upgrade
+apt-get -y dist-upgrade
 
 # get the iiab-factory
 cd 
