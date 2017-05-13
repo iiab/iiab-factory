d12ef9bb252834c8a95cae854c6e966ddd284601
George Hunt <georgejhunt@gmail.com>
2017-05-13 07:08:40 -0700

    more testing leverage if we test now something that is more like what we would wand long term

--- box/generic/setup.sh
+++ box/generic/setup.sh
@@ -31,16 +31,23 @@ popd
 # install local_vars.yml - this should be OS dependent at some point
 ./merge_local_vars.sh ../vars/stock_vars.yml
 
-# add in aliases for xs-remote-{on/off}
-grep xs-remote-on /root/.bashrc
-if [ $? -ne 0 ];then
-   cat << EOF >> /root/.bashrc
+# add in commands for xs-remote-{on/off}
+cat << EOF > /usr/local/sbin/xs-remote-on
+#!/bin/bash
+# add command to control openvpn from command line
 
-# add aliases to control openvpn from command line
-alias xs-remote-on='systemctl enable openvpn@xscenet; systemctl start openvpn@xscenet'
-alias xs-remote-off='systemctl disable openvpn@xscenet; systemctl stop openvpn@xscenet'
+systemctl enable openvpn@xscenet
+systemctl start openvpn@xscenet
 EOF
-fi  
+chmod 755 /usr/local/sbin/xs-remote-on
+cat << EOF > /usr/local/sbin/xs-remote-off
+#!/bin/bash
+# add command to control openvpn from command line
+
+systemctl disable openvpn@xscenet
+systemctl stop openvpn@xscenet
+EOF
+chmod 755 /usr/local/sbin/xs-remote-off
 
 echo 'cd /opt/schoolserver/xsce/'
 echo './runansible'
