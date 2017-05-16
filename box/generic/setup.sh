11c11072239531693bfb8fd85e6bae36c361c951
georgejhunt <georgejhunt@gmail.com>
2017-05-16 06:50:51 -0700

    Remote (#18)
    * add in xs-remote aliases to setup.sh
    
    * use here document for aliases
    
    * more testing leverage if we test now something that is more like what we would wand long term

--- box/generic/setup.sh
+++ box/generic/setup.sh
@@ -31,5 +31,23 @@ popd
 # install local_vars.yml - this should be OS dependent at some point
 ./merge_local_vars.sh ../vars/stock_vars.yml
 
+# add in commands for xs-remote-{on/off}
+cat << EOF > /usr/local/sbin/xs-remote-on
+#!/bin/bash
+# add command to control openvpn from command line
+
+systemctl enable openvpn@xscenet
+systemctl start openvpn@xscenet
+EOF
+chmod 755 /usr/local/sbin/xs-remote-on
+cat << EOF > /usr/local/sbin/xs-remote-off
+#!/bin/bash
+# add command to control openvpn from command line
+
+systemctl disable openvpn@xscenet
+systemctl stop openvpn@xscenet
+EOF
+chmod 755 /usr/local/sbin/xs-remote-off
+
 echo 'cd /opt/schoolserver/xsce/'
 echo './runansible'
