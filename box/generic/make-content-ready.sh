5148c65cff5b875a8296c6199d3b9bb56d34f180
Tim Moody <tim@timmoody.com>
2017-09-19 12:26:34 -0400

    do kalite before kiwix in case cmdsrv needs to initialize

--- box/generic/make-content-ready.sh
+++ box/generic/make-content-ready.sh
@@ -3,21 +3,21 @@
 # you must run ansible before running this
 # and reboot so hostname take effect
 # assumes kiwix, kalite are installed
-# 1. Gets the latest version of the Kiwix Catalog and rebuilds the local catalog
-# 2. Registers the Kalite install to avoid doing it manually
-# 3. Gets the Kalite English Language Pack required even for other languages
+
+# 1. Registers the Kalite install to avoid doing it manually
+# 2. Gets the Kalite English Language Pack required even for other languages
+# 3. Gets the latest version of the Kiwix Catalog and rebuilds the local catalog
+
+export KALITE_HOME=/library/ka-lite
+
+# register with kalite - just the anonymous registration
+kalite manage generate_zone
+
+# get kalite English language pack - takes awhile and seems to re-download if run again
+kalite manage retrievecontentpack download en
 
 # refresh kiwix catalog
 iiab-cmdsrv-ctl GET-KIWIX-CAT
 
 # rebuild local library.xml
 /usr/bin/iiab-make-kiwix-lib
-
-export KALITE_HOME=/library/ka-lite
-
-# register with kalite - not sure if necessary or what user/passwd to supply
-# kalite manage register - nope
-kalite manage generate_zone
-
-# get kalite English language pack - takes awhile and seems to re-download if run again
-kalite manage retrievecontentpack download en
