35f0e522f2f3628e4f711239b3ddc3114e513de8
Jerry Vonau <jvonau3@gmail.com>
2018-09-04 01:04:21 -0500

    better staging

--- scripts/continue.sh
+++ scripts/continue.sh
@@ -8,10 +8,17 @@ fi
 
 cd $BASE/iiab
 ./iiab-install
-cd $BASE/iiab-admin-console
-./install
-cd $BASE/iiab-menu
-./cp-menus
+
+if [ ! -f $BASE/iiab-admin-console/complete ]; then
+    cd $BASE/iiab-admin-console
+    ./install
+    touch $BASE/iiab-admin-console/complete
+fi
+if [ ! -f $BASE/iiab-menu/complete ]; then
+    cd $BASE/iiab-menu
+    ./cp-menus
+    touch $BASE/iiab-menu/complete
+fi
 $BASE/iiab-factory/scripts/post-install
 touch /etc/iiab/iiab-complete
 reboot
