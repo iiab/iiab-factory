303008ad8e235d1d2d5f1defc3c672e1f1052e8f
George Hunt <georgejhunt@gmail.com>
2020-03-31 14:58:58 -0700

    can fetch xml data about an upload

--- box/rpi/iiab-imager/upload.py
+++ box/rpi/iiab-imager/upload.py
@@ -7,10 +7,13 @@ import argparse
 # add path for internetarchive utility
 sys.path.append('/usr/local/lib/python2.7/dist-packages')
 import internetarchive
-import re
 from datetime import datetime
 import urllib3
 import certifi
+import glob
+import shutil
+import xml.etree.ElementTree as ET 
+
 
 # Think of the sibling files of an image file as imager_metadata
 
@@ -36,26 +39,39 @@ current_working_directory = os.getcwd()
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Upload img to archive.org, Set rpi-imager json files.")
-    parser.add_argument("-c","--check", help='Check version, update metadata.',action='store_true')
+    parser.add_argument("-c","--check", help='Check archive.org version and metadata (no changes).',action='store_true')
+    parser.add_argument("-d","--delete", help='Delete this menu item.',action='store_true')
     parser.add_argument("-r","--replace", help='Replace img.zip at archive.org.',action='store_true')
     parser.add_argument("-e","--experimental", help='Put image into Experimental menu.',action='store_true')
-    parser.add_argument("image_name", help='Specify the image file namel')
+    parser.add_argument("image_name", help='Specify the image file name')
     return parser.parse_args()
 
 
-def get_archive_org_info(identifier):
+def get_archive_org_metadata(identifier):
       return internetarchive.get_item(identifier)
+
+def get_archive_file_xml(identifier):
+   url = os.path.join(url_prefix,args.image_name,args.image_name + '_files.xml')
+   resp = (http.request("GET",url,retries=10))
+   tree = ET.fromstring(resp.data) 
+   file_data = {}
+  
+   for item in tree.findall('file'):
+      if item.attrib['name'] == args.image_name + '.zip':
+        print( item.find('md5').text)
+                   
 
 def fetch_image_info():
    md = {}
    for item in menu_names:
       fname = './%s.%s'%(args.image_name,item)
-      print(fname)
+      #print(fname)
       try:
          with open(fname,'r') as fp:
             md[item] = fp.read().rstrip()
       except Exception as e:
          print("error reading %s:%s"%(fname,e,))
+         print("Perhaps you did not run 'prepare.sh', which is required by this program")
          sys.exit(1)
    md['image_download_size'] = int(md['image_download_size'])
    md['extract_size'] = int(md['extract_size'])
@@ -64,7 +80,7 @@ def fetch_image_info():
    imager_md = md.copy()
 
 def check(name):
-   info =  get_archive_org_info(name)
+   info =  get_archive_org_metadata(name)
    if (info.metadata):
       print(str(info.metadata))
 
@@ -72,6 +88,8 @@ def main():
    global args
    global imager_menu
    args = parse_args()
+   get_archive_file_xml(args.image_name)
+   sys.exit(0)
    if args.check:
       check(args.image_name)
       sys.exit(0)
@@ -102,12 +120,12 @@ def main():
       print('Archive.org does not have file with identifier: %s'%identifier) 
       upload = True
    else:
-      print('local file md5:%s  metadata md5:%s'%(metadata['zip.md5'],item.metadata['zip_md5']))
       if item.metadata['zip_md5'] == metadata['zip.md5']:
          # already uploaded
          print('Skipping %s -- checksums match'%args.image_name)
       else:
          print('md5sums for %s do not match'%md['title'])
+         print('local file md5:%s  metadata md5:%s'%(metadata['zip.md5'],item.metadata['zip_md5']))
          upload = True
    if upload:
       # Debugging information
@@ -134,7 +152,7 @@ def do_rpi_imager():
       imager_menu = 'experimental'
    json_filename_suffix = os.path.join('os_list_imagingutility_iiab_' + imager_menu + '.json')
    json_filename = os.path.join(repo_prefix,json_filename_suffix)
-   print('json_filename:%s'%json_filename)
+   #print('json_filename:%s'%json_filename)
    try:
       with open(json_filename,'r') as fp:
          json_str = fp.read()
@@ -147,12 +165,23 @@ def do_rpi_imager():
       print("%s img.json parse error"%json_filename)
       sys.exit(1)
    data['os_list'].insert(0,metadata)
-   fname = os.path.join(repo_prefix,'output',json_filename_suffix)
-   with open (fname,'w') as fp:
+
+   # write the new json to /tmp and compare with previous version
+   fname = os.path.join(repo_prefix,json_filename_suffix)
+   tmp_name = os.path.join('/tmp',json_filename_suffix)
+   with open (tmp_name,'w') as fp:
       json.dump(data,fp,indent=2)
 
+   # Before changing the json file, make a backup copy, in case things go wrong
+   now = datetime.now()
+   date_time = now.strftime("%m-%d-%Y_%H:%M")
+   os.makedirs("./logs/%s"%date_time)
+   for fn in glob.glob(repo_prefix+'/os_list*'): 
+      shutil.copy(fn,"./logs/%s"%date_time)
+   sys.exit(1)
+   
+
 if __name__ == "__main__":
-   if not os.path.exists(repo_prefix +'/output'):
-      os.mkdir(repo_prefix +'/output')
-    # Run the main routine
+   if not os.path.exists(repo_prefix +'/logs'):
+      os.mkdir(repo_prefix +'/logs')
    main()
