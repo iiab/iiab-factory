59550c993e8e6193f806be14d3bc73e6e5c4aa7a
George Hunt <georgejhunt@gmail.com>
2020-04-01 18:13:26 -0700

    remove last vestiges of georgejhunt repo

--- box/rpi/iiab-imager/top_level.py
+++ box/rpi/iiab-imager/top_level.py
@@ -7,7 +7,7 @@ import urllib3
 import certifi
 
 src_url = "https://downloads.raspberrypi.org/os_list_imagingutility.json"
-iiab_url = "https://raw.githubusercontent.com/georgejhunt/iiab-factory/testiiab/box/rpi/iiab-imager/os_list_imagingutility_iiab.json"
+iiab_url = "https://raw.githubusercontent.com/iiab/iiab-factory/master/box/rpi/iiab-imager/os_list_imagingutility_iiab.json"
 http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',\
            ca_certs=certifi.where())
 resp = (http.request("GET",src_url,retries=10))
@@ -25,14 +25,14 @@ if True:
    iiab_json['name'] = "IIAB -- Experimental"
    iiab_json["description"] = "Pre-release and specialized Images"
    iiab_json["icon"] = "https://raw.githubusercontent.com/iiab/iiab-factory/master/box/rpi/rpi-imager/iiab40.png"
-   iiab_json["subitems_url"] = "https://raw.githubusercontent.com/georgejhunt/iiab-factory/testiiab/box/rpi/iiab-imager/os_list_imagingutility_iiab_experimental.json"
+   iiab_json["subitems_url"] = "https://raw.githubusercontent.com/iiab/iiab-factory/master/box/rpi/iiab-imager/os_list_imagingutility_iiab_experimental.json"
    data['os_list'].insert(0,iiab_json)
 
    iiab_json = {}
    iiab_json['name'] = "IIAB -- Releases"
    iiab_json["description"] = "Internet in a Box Images"
    iiab_json["icon"] = "https://raw.githubusercontent.com/iiab/iiab-factory/master/box/rpi/rpi-imager/iiab40.png"
-   iiab_json["subitems_url"] = "https://raw.githubusercontent.com/georgejhunt/iiab-factory/testiiab/box/rpi/iiab-imager/os_list_imagingutility_iiab_subitems.json"
+   iiab_json["subitems_url"] = "https://raw.githubusercontent.com/iiab/iiab-factory/master/box/rpi/iiab-imager/os_list_imagingutility_iiab_subitems.json"
    data['os_list'].insert(0,iiab_json)
    with open("os_list_imagingutility.json",'w') as fp:
       json.dump(data,fp,indent=2)
