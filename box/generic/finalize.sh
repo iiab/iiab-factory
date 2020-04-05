3c96cb791103b28ea8a0d850a5b1e54152b08a6d
George Hunt <georgejhunt@gmail.com>
2020-04-04 20:26:21 -0700

    keep iterating until the image is secure

--- box/generic/finalize.sh
+++ box/generic/finalize.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # remove all the proprietary and non generic data
 
-# use environmnt variables discovered by XSCE
-source /etc/iiab/iiab.env
 
-# the betafinalize script manages image dup tasks
-source ./betafinalize.sh
+# the secure-accounts.sh  script renives developer credentials
+source ./secure-accounts.sh
+rm -f /root/.netrc
 
 # if this is a Raspberry Pi GUI pixel version (think young kids) -nuc history
 if [ -f /etc/lightdm/lightdm.conf -a "$PLATFORM" = "raspbian" ]; then
@@ -14,23 +13,16 @@ if [ -f /etc/lightdm/lightdm.conf -a "$PLATFORM" = "raspbian" ]; then
   history -cw
 fi
 
-rm -f /root/.ssh/*
-
 # place developers' keys enabling remote access which becomes root with sudo su
 mkdir -p /home/iiab-admin/.ssh
 cp -f ../keys/developers_authorized_keys /home/iiab-admin/.ssh/authorized_keys
 chown iiab-admin:iiab-admin /home/iiab-admin/.ssh/authorized_keys
 chmod 640 /home/iiab-admin/.ssh/authorized_keys
 
-# decided to let the expansion service handle -- works all platforms
-#grep init_resize.sh /boot/cmdline.txt
-#if [ $? -ne 0 ] &&[ "$PLATFORM" = "raspbian" ]; then
-#  cp -f ./cmdline.txt /boot/cmdline.txt
-#fi
-
 # remove any aliases we might have added
 rm -f /root/.bash_aliases
 rm -f /home/iiab-admin/.bash_aliases
+cp -f /home/pi/.bashrc /root/.bashrc
 
 # put our own aliases in place, destroying any others in the process
 cp -f bash_aliases /root/.bash_aliases
@@ -40,6 +32,5 @@ cp -f bash_aliases /root/.bash_aliases
 systemctl disable openvpn@xscenet.service
 systemctl disable openvpn
 
-cd /root
-
+rm -rf /root/tools
 
