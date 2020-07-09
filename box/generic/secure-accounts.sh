8e20347a2c3ed7731a2cd84b4b5d0bab95eabefc
George Hunt <georgejhunt@gmail.com>
2020-07-08 19:18:58 -0700

    tools I use to make an image

--- box/generic/secure-accounts.sh
+++ box/generic/secure-accounts.sh
@@ -6,24 +6,21 @@ set -e
 # Try to determin if this is raspbian
 PLATFORM=`cat /etc/*release|grep ^ID=|cut -f2 -d=`
 
-ADMIN_SSH=/home/iiab-admin/.ssh
+ADMIN_SSH=/root/.ssh
 
 mkdir -p $ADMIN_SSH
 cp ../keys/developers_authorized_keys $ADMIN_SSH/authorized_keys
 chmod 700 $ADMIN_SSH
 chmod 600 $ADMIN_SSH/authorized_keys
-chown -R iiab-admin:iiab-admin $ADMIN_SSH
+chown -R root:root $ADMIN_SSH
 
-# change the default user for raspbian pixel from pi to xsce-admin
-if [ -f /etc/lightdm/lightdm.conf ]; then
-#  sed -i -e 's/^autologin-user=pi/autologin-user=xsce-admin/' /etc/lightdm/lightdm.conf
-   sleep 1
-else
-  if [ $PLATFORM = "raspberry" ]; then
+if [ $PLATFORM = "raspberry" ]; then
     # this is a headless install -- so disable pi password login
-    sed -i -e 's/^pi\:.*/pi\:\*\:17228\:0\:99999\:\:\:\:/' /etc/shadow
-  fi
+    sed -i -e 's/^pi\:.*/pi\:\*\:17228\:0\:99999\:\:\:\:/' /etc
+    #echo -e "raspberry\nraspberry"| passwd pi
 fi
+
+echo -e "g0adm1n\ng0adm1n" | passwd iiab-admin
 
 # prevent root password
 sed -i -e 's/^root\:.*/root\:\*\:17228\:0\:99999\:\:\:\:/' /etc/shadow
