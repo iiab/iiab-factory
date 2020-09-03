a1cc3b45a827430246c3c7fd1749317d96748059
George Hunt <georgejhunt@gmail.com>
2020-09-03 15:39:16 -0700

    datestr for hoourly works

--- box/generic/fieldback/report.js
+++ box/generic/fieldback/report.js
@@ -1,6 +1,5 @@
 var moment = require('moment');
 ////    Startup Initialization
-var sql = 'select connected_time/60 as value1, tx_bytes/1000000 as value2,hour as xaxis,hour from connections';
 var label1 = "Time Min";
 var label2 = "Bytes MB"
 $( '#hour' ).addClass('chosen');
@@ -8,22 +7,35 @@ $( '#usage' ).addClass('chosen');
 
 
 ///////////   Button click action routines //////////////
-var xAxisUnit = "day"
+var xAxisUnit = "doy";
 var pdEnd = document.getElementById('pdEnd');
 var pdStart = document.getElementById('pdStart');
+var pdYear = moment().format('YYYY');
+pdEnd.value = moment().format('YYYY/MM/DD');
+pdStart.value = moment().format('YYYY/MM/DD');
+var sql = hourly()  + hourly_selector();
+var now = moment().format('YYYY/MM/DD');
+
 
 function xAxis(elem){
   var clickedId = elem.id;
   $( ".chooseAxis" ).removeClass('chosen');
   $( '#'+ clickedId ).addClass('chosen');
   xAxisUnit = clickedId;
+
   switch( clickedId ){
     case "hour":
-        sql = 'select host_num, hour,sum(connected_time),sum(tx_bytes) from connections group by year,doy,hour'; 
-       pdEnd.innerHTML - moment().format('l');
-       pdStartinnerHTML - moment().format('l');
+      xAxisUnit = "doy";
+        sql = hourly()  + hourly_selector();
+        //sql = 'select host_num, hour as xaxis,sum(connected_time) as value1,sum(tx_bytes) as value2 from connections group by year,doy,hour'; 
+       pdEnd.value = moment().format('YYYY/MM/DD');
+       pdStart.value = moment().format('YYYY/MM/DD');
+       showGraph();
        break;
      case "day":
+       xAxisUnit = "doy";
+       pdEnd = now.clone();
+       pdStart.value = pdEnd
        break;
      case "week":
        break;
@@ -33,6 +45,23 @@ function xAxis(elem){
        break;
   }
 }
+window.xAxis = xAxis;
+
+function hourly_selector(){
+      sql = "datestr >= '" + pdStart.value + "' AND datestr <= '" + pdEnd.value + "'";
+   return sql;
+}
+
+function hourly(){
+   sql = 'SELECT connected_time/60 AS value1, tx_bytes/1000000 AS value2,hour as xaxis,hour FROM connections WHERE '
+   return sql;
+}
+function redraw(){
+   sql = hourly()  + hourly_selector();
+   showGraph();
+}
+window.redraw = redraw;
+
 $(document).ready(function () {
    showGraph();
 });
@@ -41,63 +70,60 @@ $(document).ready(function () {
 //////////////// Graph.js ///////////////////////////
 function showGraph(){
     $.post("select.php?sql=" + sql,function (data){
-        dataDict = JSON.parse(data);
-        console.log(dataDict);
-        var name = [];
-        var bar1 = [];
-        var bar2 = [];
-        var bar2Max = 0;
+        if (data.length == 0){
+           alert("No data returned from sql: " + sql);
+        } else {
+           dataDict = JSON.parse(data);
+           console.log(dataDict);
+           var name = [];
+           var bar1 = [];
+           var bar2 = [];
+           var bar2Max = 0;
 
-        for (var i=0;i<dataDict.length;i++) {
-            name.push(dataDict[i].xaxis);
-            bar1.push(dataDict[i].value1);
-            var bar2Val = dataDict[i].value2
-            if ( bar2Val < bar2Max) bar2Max = bar2Val;
-            bar2.push(bar2Val);
-        }
-        // scale value2
-        var trial = bar2Max;
-        scaleFactor = 1; 
-        while (trial > 1000){
-            trial = trial / 1000;
-            var scaleFactor = scaleFactor * 1000;
-        }
-        var chartdata = {
-            labels: name,
-            datasets: [
-                {
-                    label: label1,
-                    backgroundColor: 'Blue',
-                    yAxisID: 'A',
-                    data: bar1
-                },
-                {
-                    label: label2,
-                    backgroundColor: 'Green',
-                    yAxisID: 'B',
-                    data: bar2
-                }
-            ]
-        };
+           for (var i=0;i<dataDict.length;i++) {
+               name.push(dataDict[i].xaxis);
+               bar1.push(dataDict[i].value1);
+               var bar2Val = dataDict[i].value2
+               if ( bar2Val < bar2Max) bar2Max = bar2Val;
+               bar2.push(bar2Val);
+           }
+           var chartdata = {
+               labels: name,
+               datasets: [
+                   {
+                       label: label1,
+                       backgroundColor: 'Blue',
+                       yAxisID: 'A',
+                       data: bar1
+                   },
+                   {
+                       label: label2,
+                       backgroundColor: 'Green',
+                       yAxisID: 'B',
+                       data: bar2
+                   }
+               ]
+           };
 
-        var graphTarget = $("#graphCanvas");
+           var graphTarget = $("#graphCanvas");
 
-        var barGraph = new Chart(graphTarget, {
-            type: 'bar',
-            data: chartdata,
-            options: {
-                scales: {
-                  yAxes: [{
-                    id: 'A',
-                    type: 'linear',
-                    position: 'left'
-                  }, {
-                    id: 'B',
-                    type: 'linear',
-                    position: 'right'
-                  }]
-                }
-              }
-        });
+           var barGraph = new Chart(graphTarget, {
+               type: 'bar',
+               data: chartdata,
+               options: {
+                   scales: {
+                     yAxes: [{
+                       id: 'A',
+                       type: 'linear',
+                       position: 'left'
+                     }, {
+                       id: 'B',
+                       type: 'linear',
+                       position: 'right'
+                     }]
+                   }
+                 }
+           });
+         }
   });
 }
--- box/generic/fieldback/select.php
+++ box/generic/fieldback/select.php
@@ -4,7 +4,10 @@ $db = new SQLite3('/etc/iiab/clientinfo.sqlite');
 $sql = $_REQUEST['sql'];
 //$sql = 'select host_num, hour,sum(connected_time),sum(tx_bytes) from connections group by year,doy,hour'; 
 $res = $db->query($sql);
-
+if ($res == FALSE){
+  echo ''; 
+  return;
+}
 while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
     $jsonArray[] = $row;
 }
