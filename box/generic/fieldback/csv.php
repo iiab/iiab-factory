89e558d911cc2c5702918ab8a812a55790aec8b7
George Hunt <georgejhunt@gmail.com>
2020-09-11 10:52:41 -0700

    add in csv.php

---
+++ box/generic/fieldback/csv.php
@@ -0,0 +1,25 @@
+<?php
+
+$db = new SQLite3('/etc/iiab/clientinfo.sqlite');
+
+$res = $db->query('SELECT * FROM connections');
+
+$firstRow = TRUE;
+$response = '';
+while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
+   if ( $firstRow ){
+      $fieldNames = array_keys($row);
+      foreach($fieldNames as $name) {$response .= $name .  ',';  };
+      $response = substr($response,0,-1);
+      $response .= '<br>';
+      $firstRow = FALSE;
+   }
+   foreach($fieldNames as $name) {$response .= $row[$name] .  ',';  };
+   $response = substr($response,0,-1);
+   $response .= '<br>';
+  
+   
+}
+
+echo $response;
+?>
