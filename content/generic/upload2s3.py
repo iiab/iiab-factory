c8051f2332b21318882828a62398cb54b8eedb11
George Hunt <georgejhunt@gmail.com>
2020-12-06 05:07:03 +0000

    successfully uploaded file from directory

--- content/generic/upload2s3.py
+++ content/generic/upload2s3.py
@@ -12,13 +12,16 @@ import glob
 import boto3
 import botocore
 from botocore.errorfactory import ClientError
+import threading
+import hashlib
+
 #import pdb; pdb.set_trace()
 
 # Globals
 WORKING_DIR = "/library/working/maps"
 SOURCE_DIR = "/library/www/html/internetarchive"
 SPACE_NAME = "iiab"
-PREFIX = "/osm-tiles"
+PREFIX = "osm-tiles"
 ENDPOINT_URL = "https://sfo2.digitaloceanspaces.com" 
 key = os.environ['DIGITAL_OCEAN_IIAB_UPLOADER_ACCESS_KEY']
 secret = os.environ['DIGITAL_OCEAN_IIAB_UPLOADER_SECRET_KEY']
@@ -45,18 +48,61 @@ config = {
 #cors.put(CORSConfiguration=config)
 #cors.delete()
 
+def digest(fname,algorithm):
+    print(algorithm)
+    hasher = hashlib.new(algorithm)
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+           hasher.update(chunk)
+    return hasher.hexdigest()
+
 def s3_get_metadata(fname):
-    info = client.head_object(Bucket="iiab", Key="upload2s3.py")
+    try:
+        info = client.head_object(Bucket="iiab", Key=fname)
+    except Exception as e:
+        print(str(e))
+        return {}
     data = info['Metadata']
-    # print(str(data))
-    k = list(data.keys())[0]
-    v = data[k]
-    meta = {}
-    for nibble in v.split('/'):
-        key, value = nibble.split(':')
-        meta[key] = value
+    print('Returned metadata: %s'%str(data))
+    if data.get('md5','') != '':
+        meta = data
+    else:
+        # s3cmd creates metadata as a set of value pairs separated by '/'
+        meta = {}
+        for nibble in v.split('/'):
+            key, value = nibble.split(':')
+            meta[key] = value
     return meta
     
+def upload_file_to_s3(fname,meta,folder=''):
+    if len(folder) > 0 and folder[-1] != '/':
+        folder += '/'
+    try:
+        client.upload_file(fname,'iiab',folder + fname,
+            ExtraArgs={'Metadata': meta},
+            Callback=ProgressPercentage(fname))
+    except Exception as e:
+        print(e)
+        return False
+    return True
+    
+class ProgressPercentage(object):
+    def __init__(self, filename):
+        self._filename = filename
+        self._size = float(os.path.getsize(filename))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+    def __call__(self, bytes_amount):
+        # To simplify, assume this is hooked up to a single filename
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%)" % (
+                    self._filename, self._seen_so_far, self._size,
+                    percentage))
+            sys.stdout.flush()
+
 # check for presence of list of files to upload
 # if missing, create a list of all mbtiles in SOURCE_DIR
 # This lets user to delete and narrow down for testing
@@ -66,13 +112,26 @@ if not os.path.exists("%s/upload.list"%WORKING_DIR):
             #print(os.path.basename(tile))
             fp.write(tile + '\n')
 with open("%s/upload.list"%WORKING_DIR,"r") as fp:
+    os.chdir(SOURCE_DIR)
     for line in fp.readlines():
         if line.rstrip() == '':continue
         print(line)
         line = line.rstrip()
         # Check to see if the file has already been transferred
-        metadata =s3_get_metadata(os.path.basename(line))
+        metadata =s3_get_metadata('%s/%s'%(PREFIX,os.path.basename(line)))
+        #print(metadata)
         if len(metadata) != 0:
             print('file %s is already uploaded'%line)
         else:
             print('%s is missing from bucket'%line)
+            local_md = {}
+            local_md['md5'] = digest(line,'md5')
+            local_md['sha256'] = digest(line,'sha256')
+            local_md['size'] = str(os.stat(line).st_size)
+            local_md['ctime'] = str(os.stat(line).st_ctime)
+            if not os.path.exists(line): 
+                print('Warning. . . file does not exist:%s'%line)
+                continue
+            result = upload_file_to_s3(os.path.basename(line),local_md,'osm-tiles')
+            if not result:
+                sys.exit(1)
