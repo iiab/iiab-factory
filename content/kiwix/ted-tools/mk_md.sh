7ec54d26d927ab43708aecd3f3e8f17c43cef455
George Hunt <georgejhunt@gmail.com>
2021-03-24 20:49:14 -0700

    add markdown

--- content/kiwix/ted-tools/mk_md.sh
+++ content/kiwix/ted-tools/mk_md.sh
@@ -1,2 +1,8 @@
-jupyter nbconvert zimshrink.ipynb --to markdown 
+#!/bin/bash 
+# write jupyter notebook to markdown
+if [ $# -eq 0 ];then
+	echo Please select a notebook as first paramater.
+	exit 1
+fi
+jupyter nbconvert $1 --to markdown 
 
