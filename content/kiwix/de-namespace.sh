3b3773c3ed842c45ce65818a7d7021ee717ad156
George Hunt <georgejhunt@gmail.com>
2021-04-17 18:21:39 +0000

    change de-namespace to work with multiple projects

--- content/kiwix/de-namespace.sh
+++ content/kiwix/de-namespace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x 
-# dump param #1 (zim in CWD or absolute path) to work area,and de-namespace
+# dump param #1 (zim in CWD or absolute path) to dest (PREFIX/$NAME)
 set -e
 
 # Must supply ZIM
@@ -15,39 +15,39 @@ if [ ! -f $1 ];then
 fi
 
 # for use in jupyter notebook, do not overwrite any tree contents
-contents=$(ls $HOME/zims/$2/tree|wc -l)
+contents=$(ls $2/tree|wc -l)
 if [ $contents -ne 0 ];then
-    echo "The $HOME/zims/$2/tree is not empty. Delete if you want to repeat this step."
+    echo "The $2/tree is not empty. Delete if you want to repeat this step."
     exit 0
 fi
 
 # Delete the previous contents of zims
-rm -rf $HOME/zims/$2/tree
+rm -rf $2/tree
 # Make directory
-mkdir -p $HOME/zims/$2/tree
-echo "This de-namespace file reminds you that this folder will be overwritten?" > $HOME/zims/$2/tree/de-namespace
+mkdir -p $2/tree
+echo "This de-namespace file reminds you that this folder will be overwritten?" > $2/tree/de-namespace
 
-zimdump dump --dir=$HOME/zims/$2/tree $1
+zimdump dump --dir=$2/tree $1
 
 # stop here to look around at the clean dumped format
 # It looks like just living with the namespace layout imposed by zim spec might be a better strategy
 #exit 0
 
 # put all of the images back in their original places
-mv $HOME/zims/$2/tree/I/* $HOME/zims/$2/tree
+mv $2/tree/I/* $2/tree
 if [ -d I ];then
    rmdir I
 fi
 
 # Clip off the A namespace for html
-cp -rp $HOME/zims/$2/tree/A/* $HOME/zims/$2/tree
-cp -rp $HOME/zims/$2/tree/-/* $HOME/zims/$2/tree
+cp -rp $2/tree/A/* $2/tree
+cp -rp $2/tree/-/* $2/tree
 
-if [ -d $HOME/zims/$2/tree/A ];then
-   rm -rf $HOME/zims/$2/tree/A
+if [ -d $2/tree/A ];then
+   rm -rf $2/tree/A
 fi
 
-cd $HOME/zims/$2/tree
+cd $2/tree
 for f in $(find .|grep -e html -e css); do
    sed -i -e's|../../../I/||g' $f
    sed -i -e's|../../I/||g' $f
@@ -55,6 +55,6 @@ for f in $(find .|grep -e html -e css); do
    sed -i -e's|../../-/||g' $f
    sed -i -e's|../A/||g' $f
 done
-for f in $(find $HOME/zims/$2/tree -maxdepth 1 -type f );do
+for f in $(find $2/tree -maxdepth 1 -type f );do
    sed -i -e's|../-/||g' $f
 done
